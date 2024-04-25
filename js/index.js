1be3cb92dd412329514da108e7529b37b29a9948
habbiner <habbiner.andrade@gmail.com>
2024-04-24 21:24:31 -0300

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            produto: {nome: 'Vestido', valor: 100}
+        };
+    },
+
+    methods: {
+        
+    }
+
+}).mount("#app");
