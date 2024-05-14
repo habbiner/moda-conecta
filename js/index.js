cdfb8dd381615e170db73faf284fbacc190b5825
habbiner <habbiner.andrade@gmail.com>
2024-05-13 21:15:12 -0300

    Revert "Merge branch 'gabriel-teste-paginas'"
    This reverts commit c18e0ba02bff86b2bf8a27523cd92e324c166673, reversing
    changes made to fdc4711b3bce3d3b0c16548f534f5ed4f74c6ca7.

--- js/index.js
+++ js/index.js
@@ -1,91 +1,14 @@
-const tabelaDeTamanhos = {
-    template: `
-    <div class="div-img-tabela-tamanhos">
-        <img src="/img/Tabela de medidas 1.png" width="100%" height="100%">
-    </div>
-    `
-}
+const { createApp } = Vue;
 
-const informacoes = {
-    template: `
-    <div class="info">
-        <h1>Calça Feminina Pina</h1>
-        <ul>
-            <li>Apresentam um corte moderno e detalhes de acabamento cuidadosamente elaborados.</li>
-        </ul>
-        <ul>
-            <li>São versáteis, podendo ser usadas em eventos formais ou situações casuais.</li>
-        </ul>
-        <ul>
-            <li>Consideradas uma opção eco-friendly devido ao uso de um tecido sustentável e de origem natural.</li>
-        </ul>
-    </div>
-    `
-}
-
-const descricao = {
-    template: `
-    <div class="descricao">
-        <h1>Calça Feminina Pina</h1>
-        <p>As calças de pina são uma peça de vestuário
-        distintiva e elegante, feitas de um tecido de alta qualidade
-        . Pina é conhecido por sua durabilidade
-        e suavidade ao toque, resultando em um ajuste confortável
-        e luxuoso.</p>
-    </div>
-    `
-}
-
-const app = Vue.createApp({
+createApp({
     data() {
-    return {
-        componenteAtual: "informacoes",
-        tamanhos: ['P', 'M', 'G'],
-        tamanhoSelecionado: null,
-        corSelecionada: null,
-        sacolacheia: null,
-        imagemPrincipal: "/img/Roupa 1.png",
-        Imgsacola: "/img/Sacola.png",
-        mostrarLista: false
-    }
+        return {
+            produto: {nome: 'Vestido', valor: 100}
+        };
     },
+
     methods: {
-        alterarPTabelaDeTamanhos() {
-        this.componenteAtual = (this.componenteAtual === "tabelaDeTamanhos") ? "tabelaDeTamanhos" : "tabelaDeTamanhos"
-    },
-
-        alterarComponenteinfo() {
-        this.componenteAtual = (this.componenteAtual === "informacoes") ? "informacoes" : "informacoes"
-    },
-
-        alterarPDescricao() {
-        this.componenteAtual = (this.componenteAtual === "descricao") ? "descricao" : "descricao"
-    },
-
-        adicionarBorda(indice) {
-        this.tamanhoSelecionado = indice;
-    },
-        adicionarBorda1(indice) {
-        this.corSelecionada = indice;
-    },
-    alterarImagemPrincipal(corSelecionada) {
-        if (corSelecionada === 1) {
-            this.imagemPrincipal = "/img/Roupa 1.png"; 
-        } else if (corSelecionada === 2) {
-            this.imagemPrincipal = "/img/Roupa 1roxa.png";
-        }
-    },
-    sacolacombolavermeia(sacolacheia) {
-        this.Imgsacola = "/img/Sacola1.png"
+        
     }
-},
-    components: {
-        tabelaDeTamanhos,
-        informacoes,
-        descricao
-    }
-});
 
-
-
-app.mount('#app');
+}).mount("#app");
