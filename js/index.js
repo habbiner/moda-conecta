c686ca80eb81762760ef6f17534dd4214aea29ac
GabrielFillip <gabrileux@gmail.com>
2024-05-18 13:39:04 -0300

    voltando o js da ellen fdsfasdfasdgdas

--- js/index.js
+++ js/index.js
@@ -1,91 +1,61 @@
-const tabelaDeTamanhos = {
-    template: `
-    <div class="div-img-tabela-tamanhos">
-        <img src="/img/Tabela de medidas 1.png" width="100%" height="100%">
-    </div>
-    `
-}
-
-const informacoes = {
-    template: `
-    <div class="info">
-        <h1>Calça Feminina Pina</h1>
-        <ul>
-            <li>Apresentam um corte moderno e detalhes de acabamento cuidadosamente elaborados.</li>
-        </ul>
-        <ul>
-            <li>São versáteis, podendo ser usadas em eventos formais ou situações casuais.</li>
-        </ul>
-        <ul>
-            <li>Consideradas uma opção eco-friendly devido ao uso de um tecido sustentável e de origem natural.</li>
-        </ul>
-    </div>
-    `
-}
-
-const descricao = {
-    template: `
-    <div class="descricao">
-        <h1>Calça Feminina Pina</h1>
-        <p>As calças de pina são uma peça de vestuário
-        distintiva e elegante, feitas de um tecido de alta qualidade
-        . Pina é conhecido por sua durabilidade
-        e suavidade ao toque, resultando em um ajuste confortável
-        e luxuoso.</p>
-    </div>
-    `
-}
-
-const app = Vue.createApp({
-    data() {
-    return {
-        componenteAtual: "informacoes",
-        tamanhos: ['P', 'M', 'G'],
-        tamanhoSelecionado: null,
-        corSelecionada: null,
-        sacolacheia: null,
-        imagemPrincipal: "/img/Roupa 1.png",
-        Imgsacola: "/img/Sacola.png",
-        mostrarLista: false
-    }
+new Vue({
+    el: '#app',
+    data: {
+        mostrarDiv1: true,
+        mostrarDiv3: true,
+        mostrarDiv5: true, 
+        mostrarDiv6: true, 
+        mostrarDiv7: true, 
+        mostrarDiv4: false,
+        mostrarPix: false,
+        mostrarCartaoCredito: false,
+        mostrarCartaoDebito: false
+        
     },
     methods: {
-        alterarPTabelaDeTamanhos() {
-        this.componenteAtual = (this.componenteAtual === "tabelaDeTamanhos") ? "tabelaDeTamanhos" : "tabelaDeTamanhos"
-    },
-
-        alterarComponenteinfo() {
-        this.componenteAtual = (this.componenteAtual === "informacoes") ? "informacoes" : "informacoes"
-    },
-
-        alterarPDescricao() {
-        this.componenteAtual = (this.componenteAtual === "descricao") ? "descricao" : "descricao"
-    },
-
-        adicionarBorda(indice) {
-        this.tamanhoSelecionado = indice;
-    },
-        adicionarBorda1(indice) {
-        this.corSelecionada = indice;
-    },
-    alterarImagemPrincipal(corSelecionada) {
-        if (corSelecionada === 1) {
-            this.imagemPrincipal = "/img/Roupa 1.png"; 
-        } else if (corSelecionada === 2) {
-            this.imagemPrincipal = "/img/Roupa 1roxa.png";
+        trocarDiv() {
+            if (this.mostrarDiv1) {
+                this.mostrarDiv1 = false;
+                this.mostrarDiv3 = false;
+                this.mostrarDiv4 = true
+                 // Você precisa redefinir mostrarDiv4 aqui, caso contrário, ele permanecerá falso
+            } else {
+                this.mostrarDiv3 = false;
+                this.mostrarDiv1 = true;
+                this.mostrarDiv4 = false // Caso contrário, a próxima vez que trocar, mostrarDiv1 nunca será verdadeira novamente
+            }
+        },
+        trocarDivs1() {
+            this.mostrarDiv3 = false;
+            this.mostrarDiv5 = false;
+            this.mostrarDiv4 = false
+            
+        },
+        trocarDivs2() {
+            this.mostrarDiv3 = false;
+            this.mostrarDiv5 = true;
+            this.mostrarDiv6 = false;
+            this.mostrarDiv7 = false;
+            this.mostrarDiv8 = true;
+        },
+        toggleMostrarInformacoesPagamento(secao) {
+            // Definir qual seção deve ser mostrada com base no parâmetro
+            if (secao === 'pix') {
+                // Mostrar a seção do Pix
+                this.mostrarPix = true;
+                this.mostrarCartaoCredito = false;
+                this.mostrarCartaoDebito = false;
+            } else if (secao === 'credito') {
+                // Mostrar a seção do Cartão de Crédito
+                this.mostrarPix = false;
+                this.mostrarCartaoCredito = true;
+                this.mostrarCartaoDebito = false;
+            } else if (secao === 'debito') {
+                // Mostrar a seção do Cartão de Débito
+                this.mostrarPix = false;
+                this.mostrarCartaoCredito = false;
+                this.mostrarCartaoDebito = true;
+            }
         }
-    },
-    sacolacombolavermeia(sacolacheia) {
-        this.Imgsacola = "/img/Sacola1.png"
-    }
-},
-    components: {
-        tabelaDeTamanhos,
-        informacoes,
-        descricao
     }
 });
-
-
-
-app.mount('#app');
